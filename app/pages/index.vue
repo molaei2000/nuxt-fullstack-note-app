add798d6f4786a4016a985663faf66f06c845d67
triplem <mahdi.triplem@gmail.com>
2025-09-23 16:12:23 +0330

    start config

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-4">
-    <NuxtWelcome />
+  <div class="container mx-auto px-4 bg-primary">
+    <p class="text-primary">index</p>
   </div>
 </template>
