3e18faa6c5ed761415f396e27134bc0bc69d86aa
triplem <mahdi.triplem@gmail.com>
2025-10-04 15:26:13 +0330

    set favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,19 @@ export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
     css: ["~/assets/css/main.css"],
+    app: {
+        head: {
+            title: "Nuxt Fullstack Note App",
+            link: [{ rel: "icon", type: "image/x-icon", href: "/fav.png" }],
+            meta: [
+                {
+                    name: "description",
+                    content:
+                        "A fullstack note-taking application built with Nuxt.js",
+                },
+            ],
+        },
+    },
 
     runtimeConfig: {
         jwtSecret: process.env.JWT_SECRET,
